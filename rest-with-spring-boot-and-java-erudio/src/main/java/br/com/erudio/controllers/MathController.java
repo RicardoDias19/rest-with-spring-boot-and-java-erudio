b32c10bd10503b093744b0127018d00e3c3d9d9b
Ricardo Dias <ricardo.rochadias@gmail.com>
2025-11-09 17:12:05 -0300

    Adicionando código até a aula 0507

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
@@ -0,0 +1,94 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.exception.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    // http://localhost:8080/math/sum/3/5
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws IllegalArgumentException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/subtraction/3/5
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws IllegalArgumentException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/multiplication/3/5
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws IllegalArgumentException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/division/3/5
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws IllegalArgumentException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        Double denominator = convertToDouble(numberTwo);
+        if (denominator == 0)
+            throw new UnsupportedMathOperationException("Division by zero! Try another value for second number.");
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/mean/3/5
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws IllegalArgumentException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2.0;
+    }
+
+    // http://localhost:8080/math/squareroot/3/5
+    @RequestMapping("/squareroot/{numberOne}")
+    public Double squareRoot(
+            @PathVariable("numberOne") String numberOne
+    ) throws IllegalArgumentException {
+        if (!isNumeric(numberOne))
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        return Math.sqrt(convertToDouble(numberOne));
+    }
+
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
+        if (strNumber == null || strNumber.isEmpty())
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/ExceptionResponse.java
@@ -0,0 +1,6 @@
+package br.com.erudio.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public record ExceptionResponse(Date timestamp, String message, String details) implements Serializable {}
